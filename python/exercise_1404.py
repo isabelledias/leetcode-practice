ff6d11ecc35816b6d3b684fd864b33724b477be5
Isabelle Dias <isabelle.dias@industriall.ai>
2024-05-30 20:54:38 -0300

    [python-1404] Number of Steps to Reduce a Number in Binary Representation to One

---
+++ python/exercise_1404.py
@@ -0,0 +1,28 @@
+"""
+1404. Number of Steps to Reduce a Number in Binary Representation to One
+
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/description/
+"""
+
+class Solution:
+    def numSteps(self, s: str) -> int:
+        counter = 0
+        while s  != '1':
+            counter += 1
+            if s[-1] == '0':
+                s = s[:-1]
+                continue
+            else:
+                idx = s.rfind('0')
+                if idx != -1:
+                    s = s[:idx] + '1' + (len(s)-idx-1)*'0'
+                else:
+                    s = '1' + len(s)*'0'
+        return counter
+            
+            
+if __name__ == "__main__":
+    # Testes
+    assert Solution().numSteps("1101") == 6
+    assert Solution().numSteps("10") == 1
+    assert Solution().numSteps("1") == 0
