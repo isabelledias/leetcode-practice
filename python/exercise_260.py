ec5717643fb6532769b8aa63df9387ed1c51be50
Isabelle Dias <isabelle.dias@industriall.ai>
2024-05-31 00:28:40 -0300

    [python-260] Single Number III - using only constant extra space

--- python/exercise_260.py
+++ python/exercise_260.py
@@ -10,13 +10,19 @@ class Solution:
         """Given an integer array nums, in which exactly two elements appear only once and all
         the other elements appear exactly twice. Find the two elements that appear only once. 
         You can return the answer in any order."""
-        double_nums = nums.copy()
-        _ = [double_nums.remove(x) for x in set(nums)]
-        return list(set(nums) - set(double_nums))
-        
-
+        nums.sort()
+        a = 0
+        b = 0
+        for num in nums:
+            if a^num == num or a^num == 0:
+                a^=num
+                continue
+            b^=num
+        return [a, b]
+    
 if __name__ == "__main__":
     s = Solution()
     assert s.singleNumber([1,2,1,3,2,5]) in [[3, 5], [5, 3]]
     assert s.singleNumber([0, 1]) in [[0, 1], [1, 0]]
     assert s.singleNumber([-1, 0]) in [[-1,0], [0, -1]]
+    assert s.singleNumber([1403617094,-490450406,-1756388866,-967931676,1878401007,1878401007,-74743538,1403617094,-1756388866,-74743538,-490450406,-1895772685]) in [[-967931676, -1895772685], [-1895772685,-967931676]]
